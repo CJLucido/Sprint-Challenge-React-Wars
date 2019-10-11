236a10e9fb2a809cf8773845c33b107314739033
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-11 09:47:01 -0700

    got styled-components to work but only for App level, at first it was working for Person level. think I may have downloaded it at two directories and that is causing a problem

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,16 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from "axios";
+import styled from "styled-components";
+import Person from "./components/Person"
 
 
 const App = () => {//{id}
+
+const StyledApp = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 5%
+
+`
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [character, setCharacter] = useState(1)
+  const [character, setCharacter] = useState(1);
+  const [birth_year, setBirthYear] = useState(1);
+  const [hair_color, setHairColor] = useState(1);
+  const [height, setHeight] = useState(1);
+  const [mass, setMass] = useState(1);
 
 
   useEffect(() => {
@@ -27,16 +41,30 @@ const App = () => {//{id}
     .then(response => {
       console.log(response)
       let info = response.data;
-      setCharacter(info.name)
+      setCharacter(info.name);
+      setBirthYear(info.birth_year)
+      setHairColor(info.hair_color)
+      setHeight(info.height)
+      setMass(info.mass)
+    })
+    .catch(err => {
+      console.log("Give in to your feelings", err)
     })
 
   }, []) //id
+
+  axios.get(`https://swapi.co/api/people`)
+    .then(response => {
+      let pathChar = response.data.url;
+    })
  
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <h2>{character}</h2>
+      <StyledApp>
+      <Person character ={character} birth_year= {birth_year} hair_color= {hair_color} height={height} mass={mass}/>
+      </StyledApp>
     </div>
   );
 }
---
+++ starwars/src/components/Person.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+
+const StyledPerson = styled.div`
+background-color: white;
+    border-radius: 22px;
+    width: 25%;
+
+
+
+`
+
+
+
+function Person(props) {
+
+    return(
+        <StyledPerson>
+            <h2>{props.character}</h2>
+            <p>{props.birth_year}</p>
+            <p>{props.hair_color}</p>
+            <p>{props.height}</p>
+            <p>{props.mass}</p>
+        </StyledPerson>
+
+    )
+
+}
+
+export default Person;
